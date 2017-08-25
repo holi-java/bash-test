7856600d5689b530e416ab6358eb078565e0d231
holi-java <holi-java@163.com>
2017-08-26 00:33:55 +0800

    test for break & continue loop

--- test/loops.test.sh
+++ test/loops.test.sh
@@ -34,3 +34,33 @@ test_for_loop_in_a_list(){
 
   assertEquals ";hello;world" "$out"
 }
+
+test_breaking_loop(){
+  n=0
+  out="$n"
+
+  while [ true ]; do
+    if ! [ $n -lt 3 ]; then break; fi
+
+    let n++
+    out="$out;$n"
+  done
+
+  assertEquals "0;1;2;3" "$out"
+}
+
+test_continue_loop(){
+  n=0
+  out="$n"
+
+  while [ $n -lt 3 ]; do
+    let n++
+    let even="$n % 2"
+    
+    if [ $even == 0 ]; then continue; fi
+
+    out="$out;$n"
+  done
+
+  assertEquals "0;1;3" "$out"
+}
