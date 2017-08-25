20c15795d5fccbba510d0e2df324b1970a3f9d72
holi-java <holi-java@163.com>
2017-08-25 23:31:27 +0800

    test logic operators

---
+++ test/logic-operators.test.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+test_empty(){
+  assertFalse '[ ]'
+}
+
+test_not(){
+  assertTrue '[ true ]'
+  assertFalse '! [ true ]'
+}
+
+test_and(){
+  assertTrue '[ true ] && [ true ]' 
+  assertFalse '[ ] && [ true ]' 
+  assertFalse '[ true ] && [ ]' 
+  assertFalse '[ ] && [ ]' 
+}
+
+test_or(){
+  assertTrue '[ true ] || [ true ]' 
+  assertTrue '[ ] || [ true ]' 
+  assertTrue '[ true ] || [ ]' 
+  assertFalse '[ ] || [ ]' 
+}
+
+test_nested_and(){
+  assertFalse '[ true && true ]' 
+  assertFalse '[ true && false ]' 
+  assertFalse '[ false && true ]' 
+  assertFalse '[ false && false ]' 
+}
+
+test_or(){
+  assertTrue '[ true || true ]' 
+  assertTrue '[ false || true ]' 
+
+  assertFalse '[ true || false ]' 
+  assertFalse '[ false || false ]' 
+}
