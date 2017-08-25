63fcc258185f3eb052ad9e556fe008d091b37e08
holi-java <holi-java@163.com>
2017-08-25 23:54:09 +0800

    refactoring `args.sh`: replace if-elif-else satement with case statement

--- test/args.sh
+++ test/args.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
-if [ "$1" == "-f" ]; then echo $2;
-elif [ "$1" == "-c" ]; then echo `expr $# - 1`;
-elif [ "$1" == "-a" ]; then echo $@;
-elif [ "$1" == "-n" ]; then echo $0;
-elif [ "$1" == "-s" ]; then echo $?;
-elif [ "$1" == "-e" ]; then export $2=$3;
-else
-  n=`expr $1 + 1`
-  echo ${!n}
-fi
-
+case $1 in
+  -f) echo $2;;
+  -c) echo `expr $# - 1`;;
+  -a) echo $@;;
+  -n) echo $0;;
+  -s) echo $?;;
+  -e) export $2=$3;;
+  *)
+    n=`expr $1 + 1`
+    echo ${!n}
+  ;;
+esac
