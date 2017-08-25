bd6c9bedb443098fcc0a264775d15052060edaa4
holi-java <holi-java@163.com>
2017-08-25 21:13:07 +0800

    test parse expression by `expr`

---
+++ test/expr.test.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+test_parse_expression_with_no_spaces(){
+  actual=`expr 2+3`
+
+  assertEquals "2+3" $actual
+}
+
+test_parse_expression_with_spaces(){
+  actual=`expr 2 + 3`
+
+  assertEquals 5 $actual
+}
+
+test_parse_multiply_expression(){
+  actual=`expr 2 \* 3`
+
+  assertEquals 6 $actual
+}
