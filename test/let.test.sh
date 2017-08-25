b19cb8b15f1d5ca03911d9123686c4ce2e1a3f86
holi-java <holi-java@163.com>
2017-08-25 21:02:44 +0800

    test let do arithmetic operation

---
+++ test/let.test.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+test_sum_with_variables(){
+  one=1; two=2;
+
+  let sum=$one+$two
+
+  assertEquals 3 $sum
+}
+
+test_sum_with_expression(){
+  let "sum = 1 + 2"
+
+  assertEquals 3 $sum
+}
+
+test_sum_with_expression2(){
+  let sum="1 + 2"
+
+  assertEquals 3 $sum
+}
+
+test_increment(){
+  a=1
+  
+  let a++
+
+  assertEquals 2 $a
+}
+
+test_decrement(){
+  a=1
+  
+  let a--
+
+  assertEquals 0 $a
+}
+
+test_multiply(){
+  let "a = 3 * 5"
+
+  assertEquals 15 $a
+}
+
+test_use_command_substitution(){
+  assertEquals 3 "$(let a=1+2 && echo $a)"
+}
