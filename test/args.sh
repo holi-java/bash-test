efb5ddcfb067e60967e43f740d14cb13c19d9ced
holi-java <holi-java@163.com>
2017-08-25 23:00:34 +0800

    refactoring `args.sh`: replace if with if-elif-else expression

--- test/args.sh
+++ test/args.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
-if [ "$1" == "-f" ]; then echo $2 && exit; fi
-if [ "$1" == "-c" ]; then echo `expr $# - 1` && exit; fi
-if [ "$1" == "-a" ]; then echo $@ && exit; fi
-if [ "$1" == "-n" ]; then echo $0 && exit; fi
-if [ "$1" == "-s" ]; then echo $? && exit; fi
-if [ "$1" == "-e" ]; then
-  export $2=$3
+if [ "$1" == "-f" ]; then echo $2;
+elif [ "$1" == "-c" ]; then echo `expr $# - 1`;
+elif [ "$1" == "-a" ]; then echo $@;
+elif [ "$1" == "-n" ]; then echo $0;
+elif [ "$1" == "-s" ]; then echo $?;
+elif [ "$1" == "-e" ]; then export $2=$3;
 else
   n=`expr $1 + 1`
   echo ${!n}
