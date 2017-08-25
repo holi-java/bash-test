4cd1f13bc9ab6d42be4d0632302bbf16535baae2
holi-java <holi-java@163.com>
2017-08-26 04:52:29 +0800

    test append text to stream

--- test/sed.test.sh
+++ test/sed.test.sh
@@ -1,7 +1,11 @@
-#! /bin/sh
+#! /bin/bash
 
-test_replace_result_with_regex(){
+test_replace_value_by_regex(){
   assertEquals 'jest' `echo test | sed s/^./j/`
 }
 
+test_append_text_to_stream(){
+  expected="test"$'\n'"shell"
 
+  assertEquals "$expected" "`echo test | sed 'a\shell'`"
+}
