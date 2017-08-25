579f4f7abb2b968c11c9407bd30fbec2256e6ed7
holi-java <holi-java@163.com>
2017-08-25 23:44:09 +0800

    test case statement

---
+++ test/case-statement.test.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+test_case_statement(){
+  assertEquals one $(count 1)
+  assertEquals two $(count 2)
+  assertEquals many $(count 3)
+}
+
+count(){
+  case $1 in
+    1) echo one;;
+    2) echo two;;
+    *) echo many;;
+  esac
+}
