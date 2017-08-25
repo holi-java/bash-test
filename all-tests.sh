a2a51f1e6bde0a4c6e6afec9185a6f5c924e15c7
holi-java <holi-java@163.com>
2017-08-26 04:19:52 +0800

    add comments

--- all-tests.sh
+++ all-tests.sh
@@ -1,5 +1,6 @@
 shunit2="shunit2-2.1.3"
 
+# install shunit2 if needed
 if [ ! -d "$shunit2" ]; then
   curl -L "http://downloads.sourceforge.net/shunit2/$shunit2.tgz" | tar zx
 fi
@@ -13,10 +14,12 @@ done
 # search test names
 tests=`declare -F | sed s/^declare' '-f' '// | grep ^test`
 
+# add tests for shunit2
 suite(){
   for test in $tests; do
     suite_addTest $test
   done
 }
 
+# run tests
 source $shunit2/src/shell/shunit2
