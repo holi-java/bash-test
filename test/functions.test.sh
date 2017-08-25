07161522e16c42c692dcee87d06a062d6789db61
holi-java <holi-java@163.com>
2017-08-26 01:23:56 +0800

    test function scope

--- test/functions.test.sh
+++ test/functions.test.sh
@@ -1,21 +1,46 @@
 #!/bin/bash
 
 test_function_return_status_code(){
+  status(){
+    return 200
+  }
+
   status
 
   assertEquals 200 $?
 }
 
 test_function_return_result_by_echo(){
+  result(){
+    echo "bar"
+    return $1
+  }
+
   assertEquals "bar" $(result 1)
   assertEquals "bar" $(result 0)
 }
 
-status(){
-  return 200
+test_create_function_with_function_keyword(){
+  function pass { 
+    echo "PASS"
+  }
+
+  assertEquals "PASS" $(pass)
 }
 
-result(){
-  echo "bar"
-  return $1
+test_function_scope(){
+  scope(){
+    local(){
+      echo "SCOPE"
+    }
+
+    echo $(local)
+  }
+  
+  local(){ 
+    echo "LOCAL"
+  }
+
+  assertEquals "SCOPE" $(scope)
+  assertEquals "LOCAL" $(local)
 }
