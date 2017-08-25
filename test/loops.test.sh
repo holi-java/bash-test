90f4f5ecf4cee6f9935ca17c354f7014df3d8974
holi-java <holi-java@163.com>
2017-08-26 00:12:21 +0800

    test until loop

--- test/loops.test.sh
+++ test/loops.test.sh
@@ -11,3 +11,15 @@ test_while_loop(){
 
   assertEquals "0;1;2;3" "$out"
 }
+
+test_until_loop_is_negative_of_while_loop(){
+  n=0
+  out="$n"
+
+  until ! [ $n -lt 3 ]; do
+    let n++
+    out="$out;$n"
+  done
+
+  assertEquals "0;1;2;3" "$out"
+}
